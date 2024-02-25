718cd44d44c98b303bbdc8794d879a0dc8d56fe2
Salvador <sarevalotamayo@student.rtc.edu>
2024-02-24 17:22:00 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MethodsandTryParsepart1Salvador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
